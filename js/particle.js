efd7f1deeb16db83ccf03cda6a4bf71d693210fd
Antoine Buteau <antoine.buteau.1@ulaval.ca>
2014-11-06 16:25:31 -0500

    First website test with css opacity

---
+++ js/particle.js
@@ -0,0 +1,14 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+$(document).ready(function () {
+    $('body').particleground({
+        dotColor: '#000',
+        lineColor: '#000',
+        lineWidth: '0.1',
+        particleRadius: '3',
+        parallaxMultiplier: '8'
+    });
+});
